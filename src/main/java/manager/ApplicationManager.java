f1ff94f78b2df19af05b3182a47a9c32b9db6ddd
webinartelran <webinartelran@gmail.com>
2023-03-28 20:57:12 +0300

    CW15

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -2,16 +2,20 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager{
 
-    WebDriver wd;
+//    WebDriver wd;
+    EventFiringWebDriver wd;
     HelperUser user;
 
     public void init(){
-        wd = new ChromeDriver();
+//        wd = new ChromeDriver();
+        wd = new EventFiringWebDriver(new ChromeDriver());
+        wd.register(new MyListener());
         user = new HelperUser(wd);
         wd.navigate().to("https://telranedu.web.app/home");
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
